b8451846f80d049efc4be5350b10345f5740d50a
Rajat Sharda <rajatsharda@Rajats-MacBook-Air.local>
2024-04-15 22:54:23 +0530

    Started working on Dock

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import bkgrd from './assets/ui/wallpaper-day.jpg'
 import Navbar from './components/Navbar';
+import Dock from './components/Dock';
 
 function App() {
   return (
@@ -10,7 +11,9 @@ function App() {
       
       <Navbar />
       
-      
+      <div className='flex fixed bottom-0 right-0 left-0'>
+        <Dock/>
+      </div>
       
     </div>
   )
