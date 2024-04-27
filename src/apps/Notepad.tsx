4b37cd9faae8e7ed269b2343d4853b39e13a1d79
Rajat Sharda <rajatsharda@Rajats-MacBook-Air.local>
2024-04-27 23:52:59 +0530

    added resizable apps

--- src/apps/Notepad.tsx
+++ src/apps/Notepad.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Resizable, ResizableBox } from 'react-resizable';
+import Draggable, {DraggableCore} from 'react-draggable'
+import '../components/Resizable.css'
 
 const Notepad = () => {
+ 
   return (
-    <div className='text-white'>Notepad</div>
+    <div className='text-white'>
+      <ResizableBox width={800} height={400} 
+        minConstraints={[100, 100]} maxConstraints={[Infinity, Infinity]} className='bg-green-200'>
+         <div className='h-full w-full border border-red-500'>
+          Hello
+         </div>
+      </ResizableBox>  
+    </div>
   )
 }
-
 export default Notepad
