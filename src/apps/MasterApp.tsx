9b4f71285790338f100bd6891661dca62073d5a7
Rajat Sharda <rajatsharda@Rajats-MacBook-Air.local>
2024-05-18 19:41:16 +0530

    added close buttons

--- src/apps/MasterApp.tsx
+++ src/apps/MasterApp.tsx
@@ -4,6 +4,8 @@ import Draggable from 'react-draggable'
 import '../components/Resizable.css'
 import Notepad from './Notepad'
 import close from '../assets/icons/mac_close/icons8-macos-close-30.png'
+import minimize from '../assets/icons/mac_minimize/icons8-macos-minimize-30.png'
+import fullScrn from '../assets/icons/mac_FullScrn/icons8-macos-full-screen-30.png'
 
 const MasterApp = () => {
   type ResizeHandleAxis = 's' | 'w' | 'e' | 'n' | 'sw' | 'nw' | 'se' | 'ne'
@@ -30,19 +32,17 @@ const MasterApp = () => {
   return (
     <div className='text-white w-screen h-screen'>
       <div className='relative h-screen w-screen mt-8'>
-
         <Draggable
           bounds='parent'
           handle='.drag-handle'
           defaultPosition={setPos}
         >
-          <div className='absolute top-0 left-0 rounded-b-2xl shadow-md border border-gray-600 shadow-gray-600 rounded-xl'>
-            <div className='bg-gray-500 h-6 drag-handle rounded-t-xl rounded-b-none' />
-              <div className='absolute top-0 left-0 w-full'>
-                <div>
-                  <img src={close} className='h-4'/>
-                </div>
-              </div>
+          <div className='absolute top-0 left-0 shadow-md border border-gray-600 shadow-gray-600 rounded-xl'>
+            <div className='bg-gray-500 h-6 drag-handle rounded-t-xl rounded-b-none flex items-center p-1 pl-2'>
+              <img src={close} className='h-4 mr-1' />
+              <img src={minimize} className='h-4 mr-1' />
+              <img src={fullScrn} className='h-4' />
+            </div>
             <ResizableBox
               width={width}
               height={height}
@@ -50,13 +50,12 @@ const MasterApp = () => {
               maxConstraints={[Infinity, Infinity]}
               resizeHandles={['se', 's', 'e']}
               onResize={handleResize}
-              className='rounded-xl rounded-b-md'
+              className='bg-green-200 rounded-b-xl'
               lockAspectRatio={false}
             >
               <div className='flex items-center justify-center h-full w-full'>
                 <Notepad />
               </div>
-              
             </ResizableBox>
           </div>
         </Draggable>
