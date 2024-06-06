c7cf65c6b5291ab1cbba52f170dd1381a42b4e5a
Rajat Sharda <rajatsharda@Rajats-MacBook-Air.local>
2024-06-06 16:02:42 -0400

    responsive

--- src/apps/Siri.tsx
+++ src/apps/Siri.tsx
@@ -70,19 +70,19 @@ const Siri = () => {
 
   return (
     <div className="fixed h-screen w-screen bg-light-bg bg-cover text-white">
-      <div className="h-full w-full backdrop-filter backdrop-brightness-105 backdrop-blur-2xl">
-        <div className="p-20 w-full">
-          <div className="flex justify-center">
-            <div className="absolute left-0 top-0 pt-16 pl-20">
-              <img src={back} className='cursor-pointer' alt="back-button" onClick={() => dispatch(setCurrApp('Finder'))} />
+      <div className="h-full w-full backdrop-filter backdrop-brightness-105 backdrop-blur-2xl pt-10">
+        <div className="p-4 sm:p-4 custom:p-6 w-full">
+          <div className="flex items-center mb-8">
+            <div className="flex items-center p-1 ">
+              <img src={back} className='cursor-pointer h-8 w-8' alt="back-button" onClick={() => dispatch(setCurrApp('Finder'))} />
             </div>
-            <div className="text-5xl font-Apple_Bold">
+            <div className="text-4xl flex justify-center custom:flex custom:justify-center font-Apple_Bold custom:text-4xl w-full">
               Ask Siri about anything!
             </div>
           </div>
-          <div className="flex justify-center pt-6">
+          <div className="flex justify-center mb-4">
             <div className='font-Apple_Regular text-sm'>
-                <a className='pr-4'> Response : </a>
+              <span className='pr-4'>Response:</span>
               <label className="pr-4">
                 <input type="radio" value="immediate" checked={responseSpeed === 'immediate'} onChange={handleSpeedChange} />
                 Immediate
@@ -99,9 +99,9 @@ const Siri = () => {
           </div>
         </div>
       
-        <div className='fixed p-20 py-10 h-3/5 w-full'>
-          <div className='p-16 pt-16 border h-full w-full border-white rounded-3xl shadow-sm shadow-white overflow-auto'>
-            <div className='font-Apple_Regular text-xl bg-inherit space-y-2'>
+        <div className='fixed inset-x-0 top-1/4 p-4 sm:p-2 custom:p-4 h-2/3 custom:h-1/2'>
+          <div className='p-4 border h-full border-white rounded-3xl shadow-sm shadow-white overflow-auto'>
+            <div className='font-Apple_Regular text-lg bg-inherit space-y-2'>
               <div className='flex flex-col space-y-2'>
                 <SiriMessage />
                 {messages.map((msg, index) => (
@@ -109,17 +109,17 @@ const Siri = () => {
                   <UserMessage key={index} userInput={msg.text} /> : 
                   <SiriMessage key={index} message={msg.text} />
                 ))}
-                {loading && <div className="text-white"></div>}
+                {loading && <div className="text-white">Loading...</div>}
                 <div ref={messagesEndRef} />
               </div>
             </div>
           </div>
         </div>
         
-        <div className='fixed bottom-10 left-0 right-0 px-20 w-full rounded-xl font-Apple_Regular'>
+        <div className='fixed bottom-4 left-0 right-0 px-8 sm:px-4 custom:px-6 w-full'>
           <input type='text' 
             placeholder='Write anything to ask to Siri....'
-            className='rounded-xl border border-white shadow-xl bg-inherit w-full min-h-12 overflow-hidden px-3 text-lg' 
+            className='rounded-xl border border-white shadow-xl bg-inherit w-full min-h-12 px-3 text-lg custom:text-base' 
             value={input}
             onChange={handleInput} 
             onKeyDown={handleKeyDown}
