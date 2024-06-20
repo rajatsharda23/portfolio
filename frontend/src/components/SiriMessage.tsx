d3be3546370d684de272fb05fda3c065b2b26ebb
Rajat Sharda <rajatsharda@Rajats-MacBook-Air.local>
2024-06-20 16:38:09 -0400

    new resp

--- frontend/src/components/SiriMessage.tsx
+++ frontend/src/components/SiriMessage.tsx
@@ -8,8 +8,8 @@ interface SiriMessageProps {
 const SiriMessage: React.FC<SiriMessageProps> = ({ message = "Hey there, I'm Siri!", className }) => {
   return (
     <div className={`flex items-start space-x-2 ${className}`}>
-      <div className="h-10 w-10">
-        <img src={siri} alt="siriLogo" className="h-10 w-10" />
+      <div className="h-10 w-10 flex-shrink-0">
+        <img src={siri} alt="siriLogo" className="h-full w-full object-contain" />
       </div>
       <div className="flex flex-col">
         <div className="min-w-40 max-w-[75%] lg:max-w-[60%] min-h-12 p-2 pr-3 border border-gray-400 rounded-xl overflow-hidden bg-blue-900 bg-opacity-50 bg-blend-overlay">
--- frontend/src/components/UserMessage.tsx
+++ frontend/src/components/UserMessage.tsx
@@ -5,7 +5,7 @@ interface UserMessageProps {
   const UserMessage: React.FC<UserMessageProps> = ({ userInput }) => {
     return (
       <div className='flex items-center flex-row-reverse'>
-        <div className='h-10 w-10 min-h-10 min-w-10 rounded-full border border-gray-300 bg-gray-200 p-1 m-2'>
+        <div className='h-10 w-10 min-h-10 min-w-10 rounded-full border border-gray-300 bg-gray-200 p-1 m-2 flex-shrink-0'>
           <img src="https://img.icons8.com/ios-filled/100/login-as-user.png" alt="login-as-user"
             className="sticky h-8 w-8 "/>
         </div>
